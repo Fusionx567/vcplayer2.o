9a3617a794d200125a4feb0720bc5f0598cc5cdf
Fusionx567 <131298526+Fusionx567@users.noreply.github.com>
2024-08-24 10:45:56 +0530

    Update queue.py

--- BrandrdXMusic/utils/inline/queue.py
+++ BrandrdXMusic/utils/inline/queue.py
@@ -73,10 +73,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ 🥀", url=f"https://t.me/fps144hz"
+                text="⛦ ᴏᴡɴᴇʀ ", url=f"https://t.me/fps144hz"
             ),
             InlineKeyboardButton(
-                text="⛦ ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/Anime_Group_Chat_Asia"
+                text="⛦ ꜱᴜᴘᴘᴏʀᴛ ", url=f"https://t.me/Anime_Group_Chat_Asia"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
